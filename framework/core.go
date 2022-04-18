9db78a7ded0832fc22a86e158aee6d1f50230fca
sunran <sunranlike@163.com>
2022-04-18 21:25:02 +0800

    Initial commit

---
+++ framework/core.go
@@ -0,0 +1,23 @@
+
+package framework
+
+import "net/http"
+
+// 框架核心结构
+type Core struct {
+}
+
+// 初始化框架核心结构
+func NewCore() *Core {
+	return &Core{}
+}
+
+// 框架核心结构实现Handler接口，如果你不自己实现就会调用默认Handler，
+func (c *Core) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	// TODO
+}
+//在源码中Handler 接口实际上只有一个函数 就是ServerHTTP方法，所以我们自己写一个ServeHttp就代表我们
+//使用自己的Handler，（实际上是一个ServerHttp方法）
+//type Handler interface {
+//	ServeHTTP(ResponseWriter, *Request)
+//}
