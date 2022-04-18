bd4861067f25959f7d90273128640597fa969693
sunran <sunranlike@163.com>
2022-04-18 21:25:02 +0800

    Initial commit

--- framework/core.go
+++ framework/core.go
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 
+=======
+>>>>>>> 8e475c8 (Initial commit)
 package framework
 
 import "net/http"
@@ -16,8 +19,16 @@ func NewCore() *Core {
 func (c *Core) ServeHTTP(response http.ResponseWriter, request *http.Request) {
 	// TODO
 }
+<<<<<<< HEAD
+=======
+
+>>>>>>> 8e475c8 (Initial commit)
 //在源码中Handler 接口实际上只有一个函数 就是ServerHTTP方法，所以我们自己写一个ServeHttp就代表我们
 //使用自己的Handler，（实际上是一个ServerHttp方法）
 //type Handler interface {
 //	ServeHTTP(ResponseWriter, *Request)
+<<<<<<< HEAD
 //}
+=======
+//}
+>>>>>>> 8e475c8 (Initial commit)
--- main.go
+++ main.go
@@ -6,12 +6,21 @@ import (
 )
 
 func main() {
+<<<<<<< HEAD
 	server := &http.Server{//这个结构体的Handler字段就只去使用自己的Handler函数
 		// 自定义的请求核心处理函数
 		Handler: framework.NewCore(),//实际上server结构体的的一个方法接口就是Handler,但是Handler只有一个方法
 		//就是serverHTTP,只要我返回一个结构体,这个结构体有Handler这个方法,就符合这个接口
 		// 请求监听地址
 		Addr:    ":8080",
+=======
+	server := &http.Server{ //这个结构体的Handler字段就只去使用自己的Handler函数
+		// 自定义的请求核心处理函数
+		Handler: framework.NewCore(), //实际上server结构体的的一个方法接口就是Handler,但是Handler只有一个方法
+		//就是serverHTTP,只要我返回一个结构体,这个结构体有Handler这个方法,就符合这个接口
+		// 请求监听地址
+		Addr: ":8080",
+>>>>>>> 8e475c8 (Initial commit)
 	}
 	server.ListenAndServe()
 }
